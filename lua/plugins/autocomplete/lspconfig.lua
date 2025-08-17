4c83ca6d2f3b1289e10d5d76273a4538cb05d52a
wanto-production <ikwansatria3974@gmail.com>
2025-08-17 12:29:03 +0700

    initial commit

--- lua/plugins/autocomplete/lspconfig.lua
+++ lua/plugins/autocomplete/lspconfig.lua
@@ -121,8 +121,6 @@ return {
       local lspconfig = require 'lspconfig'
 
       for server, config in pairs(opts.servers) do
-        -- passing config.capabilities to blink.cmp merges with the capabilities in your
-        -- `opts[server].capabilities, if you've defined it
         config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
         lspconfig[server].setup(config)
       end
