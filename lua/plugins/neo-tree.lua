bcac6b7d4ba0d7aab9d20e442f1ca1c93a8053d0
wanto-production <ikwansatria3974@gmail.com>
2025-08-27 15:03:18 +0700

    update

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,23 +8,30 @@ return {
   },
   lazy = false, -- penting: biar load di startup
   priority = 1000,
-  opts = {
-    window = {
-      position = 'right',
-      width = 25,
-    },
-    default_component_configs = {
-      indent = { indent_size = 1, padding = 0 },
-      icon = { padding = ' ' },
-    },
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        hide_dotfiles = false,
-        hide_gitignored = false,
+  config = function()
+    require('neo-tree').setup {
+      window = {
+        position = 'right',
+        width = 25,
       },
-      follow_current_file = { enabled = true },
-      use_libuv_file_watcher = true,
-    },
-  },
+      default_component_configs = {
+        indent = { indent_size = 1, padding = 0 },
+        ---@diagnostic disable-next-line : missing-fields
+        icon = { padding = ' ' },
+      },
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+      },
+    }
+
+    vim.keymap.set('n', '<leader>e', function()
+      require('neo-tree.command').execute { toggle = true, dir = vim.fn.expand '%:p:h' }
+    end, { desc = 'Toggle Neo-tree at file dir' })
+  end,
 }
