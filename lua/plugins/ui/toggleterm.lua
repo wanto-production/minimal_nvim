6219ed089ad5fd70d2f631ffe4f5c1bada13b5bd
wanto-production <ikwansatria3974@gmail.com>
2025-09-08 19:18:53 +0700

    initial commit

--- lua/plugins/ui/toggleterm.lua
+++ lua/plugins/ui/toggleterm.lua
@@ -5,7 +5,7 @@ return {
     require('toggleterm').setup {
       shell = 'fish',
       size = 20,
-      open_mapping = [[<leader>tt]],
+      --open_mapping = [[<leader>tt]],
       hide_numbers = true,
       shade_terminals = true,
       shading_factor = 2,
@@ -15,5 +15,25 @@ return {
         winblend = 3,
       },
     }
+
+    local Terminal = require('toggleterm.terminal').Terminal
+
+    -- simpan list terminal
+    local terminals = {}
+
+    -- fungsi buat toggle terminal tertentu
+    local function toggle_term(id)
+      if not terminals[id] then
+        terminals[id] = Terminal:new { count = id, direction = 'float' } -- bisa 'horizontal' / 'vertical' juga
+      end
+      terminals[id]:toggle()
+    end
+
+    -- bikin keymap kayak bufferline
+    for i = 1, 5 do -- atur berapa maksimal terminal yang mau kamu pakai
+      vim.keymap.set('n', ('<leader>t%s'):format(i), function()
+        toggle_term(i)
+      end, { desc = 'Toggle terminal ' .. i })
+    end
   end,
 }
