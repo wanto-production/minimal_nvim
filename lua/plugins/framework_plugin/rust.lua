7007ee10da59f18c84e2a5cc7c36eae4b9e83316
wanto-production <ikwansatria3974@gmail.com>
2025-10-01 20:10:35 +0700

    initial commit

--- lua/plugins/framework_plugin/rust.lua
+++ lua/plugins/framework_plugin/rust.lua
@@ -1,46 +1,40 @@
 return {
   {
     'mrcjkb/rustaceanvim',
-    version = '^4',
-    ft = 'rust',
-    lazy = true,
-    init = function()
-      -- Aktifkan inlay hints global (sekali saja)
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('RustInlayHints', { clear = true }),
-        callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if client and client.name == 'rust-analyzer' then
-            vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+    version = '^4', -- Recommended
+    ft = { 'rust' },
+    config = function()
+      vim.g.rustaceanvim = {
+        server = {
+          on_attach = function(_, bufnr)
+            vim.lsp.inlay_hint.enable()
+
+            -- format on save rust_analyzer
+            -- https://github.com/nvimtools/none-ls.nvim/issues/58#issuecomment-1924105768
+            vim.api.nvim_clear_autocmds({ buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = bufnr })
+              end,
+            })
           end
-        end,
-      })
-    end,
-    opts = {
-      server = {
-        on_attach = function(_, bufnr)
-          -- Format on save (tanpa clear berulang)
-          local group = vim.api.nvim_create_augroup('RustFormat', { clear = false })
-          vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-          vim.api.nvim_create_autocmd('BufWritePre', {
-            buffer = bufnr,
-            group = group,
-            callback = function()
-              vim.lsp.buf.format({ bufnr = bufnr, async = true })
-            end,
-          })
-        end,
-      },
-    },
+        }
+      }
+    end
   },
+  -- crates
   {
-    'saecki/crates.nvim',
-    version = 'v0.3.0',
-    event = 'BufRead Cargo.toml', -- hanya aktif di Cargo.toml
-    ft = { 'toml' },
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = {
-      popup = {},
-    },
+    "saecki/crates.nvim",
+    version = "v0.3.0",
+    lazy = true,
+    ft = { "rust", "toml" },
+    event = { "BufRead", "BufReadPre", "BufNewFile" },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("crates").setup {
+        popup = {}
+      }
+    end,
   },
 }
