5bfc84ab8d94928f0559cf29fd1c65305406c405
wanto-production <ikwansatria3974@gmail.com>
2025-11-13 14:12:36 +0700

    update config structure

--- lua/wanto/plugins/lsp/mason.lua
+++ lua/wanto/plugins/lsp/mason.lua
@@ -1,59 +1,34 @@
 return {
-  -- Mason core
   {
-    'williamboman/mason.nvim',
-    build = ':MasonUpdate',
-    config = function()
-      require('mason').setup {
-        ui = {
-          border = 'rounded',
-          height = 0.8,
-          width = 0.8,
-          keymaps = {
-            apply_language_filter = 'f',
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      -- list of servers for mason to install
+      ensure_installed = require('users.lang').mason.lsp
+    },
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = {
+          ui = {
+            icons = {
+              package_installed = "✓",
+              package_pending = "➜",
+              package_uninstalled = "✗",
+            },
           },
-          icons = {
-            package_installed = '✓',
-            package_pending = '➜',
-            package_uninstalled = '✗',
-          },
+          registries = require("users.lang").mason.registries
         },
-        registries = require('users.lang').mason.registries,
-      }
-    end,
-  },
-
-  -- Mason LSP Config untuk auto install LSP servers
-  {
-    'williamboman/mason-lspconfig.nvim',
-    dependencies = {
-      'williamboman/mason.nvim',
-      'neovim/nvim-lspconfig',
+      },
+      "neovim/nvim-lspconfig",
     },
-    event = { 'VimEnter' },
-    config = function()
-      require('mason-lspconfig').setup {
-        ensure_installed = require('users.lang').mason.lsp,
-        automatic_installation = true,
-      }
-    end,
   },
-
-  -- Mason Tool Installer untuk tools lainnya (formatters, linters, DAP)
   {
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
-    dependencies = {
-      'williamboman/mason.nvim',
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = {
+      ensure_installed = require("users.lang").mason.formatter_and_linter,
     },
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      require('mason-tool-installer').setup {
-        ensure_installed = require('users.lang').mason.formatter_and_linter,
-        auto_update = true,
-        run_on_start = true,
-        start_delay = 3000, -- delay 3 detik setelah nvim start
-        debounce_hours = 5, -- check update setiap 5 jam
-      }
-    end,
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
   },
 }
