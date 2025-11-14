3f2cd2b1890b3f2675d201ba863275bb14b7ffa8
wanto-production <ikwansatria3974@gmail.com>
2025-11-14 18:44:58 +0700

    update

--- lua/wanto/plugins/lsp/conform.lua
+++ lua/wanto/plugins/lsp/conform.lua
@@ -1,22 +1,14 @@
 return {
   { -- Autoformat
     'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
+    event = { 'BufEnter' },
     cmd = { 'ConformInfo' },
     config = function()
       require('conform').setup {
-        notify_on_error = false,
-        format_on_save = function(bufnr)
-          local disable_filetypes = { c = true, cpp = true }
-          if disable_filetypes[vim.bo[bufnr].filetype] then
-            return nil
-          else
-            return {
-              timeout_ms = 500,
-              lsp_format = 'fallback',
-            }
-          end
-        end,
+        format_on_save = {
+          timeout_ms = 500,
+          lsp_format = 'fallback',
+        },
         formatters_by_ft = {
           lua = { 'stylua' },
           -- javascript = { "prettierd", "prettier", stop_after_first = true },
