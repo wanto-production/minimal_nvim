51b1c665ce7cd6b6c7aa959e088d736eb0510c9d
wanto-production <ikwansatria3974@gmail.com>
2025-11-14 18:45:45 +0700

    update

--- lua/wanto/plugins/snacks.lua
+++ lua/wanto/plugins/snacks.lua
@@ -103,15 +103,7 @@ return {
           --     "SnacksIndent8",
           -- },
         },
-        -- animate scopes. Enabled by default for Neovim >= 0.10
-        -- Works on older versions but has to trigger redraws during animation.
         ---@class snacks.indent.animate: snacks.animate.Config
-        ---@field enabled? boolean
-        --- * out: animate outwards from the cursor
-        --- * up: animate upwards from the cursor
-        --- * down: animate downwards from the cursor
-        --- * up_down: animate up or down based on the cursor position
-        ---@field style? "out"|"up_down"|"down"|"up"
         animate = {
           enabled = vim.fn.has 'nvim-0.10' == 1,
           style = 'out',
