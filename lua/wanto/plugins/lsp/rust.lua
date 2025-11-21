a799338d27a14b06b03da5d6c86a3c1d95a621a7
wanto-production <ikwansatria3974@gmail.com>
2025-11-21 19:37:06 +0700

    update

--- lua/wanto/plugins/lsp/rust.lua
+++ lua/wanto/plugins/lsp/rust.lua
@@ -1,81 +1,89 @@
 return {
   {
     'mrcjkb/rustaceanvim',
-    version = '^6', -- Gunakan versi terbaru
+    version = '^6',
     ft = { 'rust' },
     config = function()
       vim.g.rustaceanvim = {
-        -- Konfigurasi server
         server = {
           on_attach = function(_, bufnr)
-            -- Aktifkan inlay hints
             vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
 
-            -- Setup formatting otomatis saat save (opsional)
-            -- vim.api.nvim_clear_autocmds { buffer = bufnr }
-            -- vim.api.nvim_create_autocmd('BufWritePre', {
-            --   buffer = bufnr,
-            --   callback = function()
-            --     vim.lsp.buf.format { bufnr = bufnr }
-            --   end,
-            -- })
-
-            -- Contoh lain: setup keymap
-            -- require("utils").lsp_keymaps(bufnr)
+            vim.api.nvim_clear_autocmds { buffer = bufnr }
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format { bufnr = bufnr }
+              end,
+            })
           end,
-          -- Opsi lain untuk rust-analyzer
           settings = {
             ['rust-analyzer'] = {
-              -- Contoh: aktifkan proc macro expansion
-              procMacro = {
-                enable = true,
-              },
-              -- Aktifkan caching
-              cachePriming = {
-                enable = true,
-              },
-              -- Aktifkan indexing di background
-              indexing = {
-                enable = true,
+              inlayHints = {
+                -- Nonaktifkan parameter hints (misal: `param:` di fungsi)
+                parameterHints = { enable = false },
+
+                -- Nonaktifkan type hints (tipe variabel)
+                typeHints = { enable = true },
+
+                -- Nonaktifkan chaining hints (pada method chain)
+                chainingHints = { enable = true },
+
+                -- Nonaktifkan closure capture hints
+                closureCaptureHints = { enable = false },
+
+                -- Nonaktifkan closure return type hints
+                closureReturnTypeHints = { enable = 'always' }, -- atau "always", "with_block"
+
+                -- Nonaktifkan lifetime hints
+                lifetimeElisionHints = { enable = 'never' }, -- atau "always", "skip_trivial"
+
+                -- Nonaktifkan binding mode hints (misal: ref, ref mut)
+                bindingModeHints = { enable = true },
+
+                -- Nonaktifkan const generic parameter hints
+                genericParameterHints = {
+                  type = { enable = false },
+                  lifetime = { enable = false },
+                  const = { enable = false },
+                },
+
+                -- Nonaktifkan closing brace hints (untuk block panjang)
+                closingBraceHints = { enable = true },
+
+                -- Nonaktifkan adjustment hints (misal: deref, reborrow)
+                expressionAdjustmentHints = { enable = 'never' }, -- atau "always", "reborrow_like"
+
+                -- Nonaktifkan discriminant hints (pada enum)
+                discriminantHints = { enable = 'never' }, -- atau "always"
+
+                -- Nonaktifkan impl dyn trait hints
+                impliedDynTraitHints = { enable = true },
+
+                -- Nonaktifkan implicit drop hints
+                implicitDrops = { enable = false },
+
+                -- Nonaktifkan sized bound hints
+                implicitSizedBoundHints = { enable = false },
+
+                -- Nonaktifkan range exclusive hints (misal: ..)
+                rangeExclusiveHints = { enable = false },
+
+                -- Jangan tampilkan hint lebih dari N karakter
+                maxLength = 8, -- ubah atau set ke `nil` untuk unlimited
               },
             },
           },
         },
-        -- Opsi lainnya
         tools = {
-          -- Aktifkan rust tools lainnya
           inlay_hints = {
             auto = true,
-            show_parameter_hints = true,
-            parameter_hints_prefix = '<- ',
-            other_hints_prefix = '=> ',
-          },
-        },
-
-        dap = {
-          adapter = {
-            type = 'executable',
-            name = 'codelldb',
-            command = vim.fn.stdpath 'data' .. '/mason/bin/codelldb', -- Path ke codelldb dari Mason
-            args = { '--port', '45326' },
-          },
-          configuration = {
-            {
-              name = 'Launch',
-              type = 'codelldb',
-              request = 'launch',
-              program = function()
-                return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug/', 'file')
-              end,
-              cwd = '${workspaceFolder}',
-              stopOnEntry = false,
-              args = {},
-            },
           },
         },
       }
     end,
   },
+
   -- Tambahkan juga crates.nvim untuk manajemen dependensi
   {
     'saecki/crates.nvim',
